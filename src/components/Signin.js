024288a5ca8536350cab6210531e1b49eb37fd9f
Zahnen Garner <zahnen@gmail.com>
2021-02-26 17:09:56 -0800

    add Signin component

---
+++ src/components/Signin.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Signin(){
+
+  return (
+    <>
+      <h1>This is a test</h1>
+      <h1>To see if this component will render</h1>
+    </>
+  );
+}
+
+export default Signin;
