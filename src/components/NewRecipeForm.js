c18b73d5e673f6d3c9b3834586fef35e0306a19c
Zahnen Garner <zahnen@gmail.com>
2021-02-26 16:55:58 -0800

    add NewRecipeForm component for initial recipe creation

---
+++ src/components/NewRecipeForm.js
@@ -0,0 +1,66 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Form, Button } from 'react-bootstrap';
+import { useFirestore } from 'react-redux-firebase';
+
+
+function NewRecipeForm(props){
+  const firestore = useFirestore();
+  function addRecipeToFirestore(event) {
+    event.preventDefault();
+    props.onRecipeAdd();
+    return firestore.collection('recipes').add(
+      {
+        title: event.target.title.value,
+        author: event.target.author.value,
+        ingredients: event.target.ingredients.value,
+        instructions: event.target.instructions.value
+      }
+    );
+  }
+  return (
+    <>
+      <div className="container" style={{width: '48rem'}}>
+        <Form onSubmit={addRecipeToFirestore}>
+          <Form.Group>
+            <Form.Label>Title</Form.Label>
+            <Form.Control
+              type="text"
+              name="title"
+              placeholder="Title" />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Author</Form.Label>
+            <Form.Control
+              type="text"
+              name="author"
+              placeholder="Author Name" />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Ingredients</Form.Label>
+            <Form.Control
+              type="text"
+              name="ingredients"
+              placeholder="Ingredients" />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Instructions</Form.Label>
+            <Form.Control
+              type="text"
+              name="instructions"
+              placeholder="Instructions" />
+          </Form.Group>
+          <Button variant="success" type="submit">Add Recipe</Button>
+        </Form>
+      </div>
+    </>
+  );
+}
+
+NewRecipeForm.propTypes = {
+  onRecipeAdd: PropTypes.func,
+  formSubmissionHandler: PropTypes.func,
+  buttonText: PropTypes.string
+};
+
+export default NewRecipeForm;
