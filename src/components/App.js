0d8dfd20a04287705cf0b9014ed9753df1554fc6
Zahnen Garner <zahnen@gmail.com>
2021-02-26 16:54:40 -0800

    add app component with react-router routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import NavBar from "./NavBar";
+import SignIn from './SignIn';
+import RecipeControl from "./RecipeControl";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+  return(
+    <>
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route path = "/">
+            <div className="container">
+              <RecipeControl />
+            </div>
+          </Route>
+          <Route path = "/signin">
+            <SignIn />
+          </Route>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
