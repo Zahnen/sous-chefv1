10283dfb8ccb9a189fa15177a1ad8b2512b0f988
Zahnen Garner <zahnen@gmail.com>
2021-03-05 20:23:22 -0800

    modify Recipe display in list

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -4,15 +4,11 @@ import PropTypes from "prop-types";
 
 function Recipe(props){
 
-  const ingredientsList = props.ingredients.map((item) => <li>{item}</li>);
-  const instructionsList = props.instructions.map((item) => <li>{item}</li>);
   return (
     <>
       <div onClick = {() => props.onRecipeSelection(props.id)}>
         <h1>{props.title}</h1>
         <h2>{props.author}</h2>
-        <ul>{ingredientsList}</ul>
-        <ul>{instructionsList}</ul>
       </div>
     </>
   )
